caaad05b8cda4fa9b989121299f06743320376f4
martinlikerost <marknu05@skole.mrfylke.no>
2026-03-19 14:39:36 +0100

    19.03.2026 veldig kul update

--- index.js
+++ index.js
@@ -24,3 +24,38 @@ function opentab(evt, tabname) {
   document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".scmbtn").click();
   });
+  //down below is the code you want martin
+  //starting the next table i just want you to know by having three comments
+  // final comment ends now too
+
+
+
+
+function opentab2(evt, tabname) {
+
+  var i, tabcontent2, scmbtn; // tabcontent will be replacing btncontent and scmbtn replaces navbtn
+
+  // - 1
+  tabcontent2 = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent2.length; i++) {
+    tabcontent2[i].style.display = "none";}
+
+    //- 2
+  scmbtn2 = document.getElementsByClassName("scmbtn2"); //- originally navbtn
+  for (i = 0; i < scmbtn2.length; i++) {
+    scmbtn2[i].className = scmbtn2[i].className.replace(" active", "");
+  }
+
+  //this code shows the selected code 3
+  document.getElementById(tabname2).style.display = "block";
+
+  //this code supposedly marks it as active 4
+  evt.currentTarget.className += " active";
+}
+
+  //this just notes that clicking the navbtn is an event 5
+  document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".scmbtn2").click();
+  });
+
+  
